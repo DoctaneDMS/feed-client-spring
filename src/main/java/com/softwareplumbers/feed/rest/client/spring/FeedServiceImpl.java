72ff5e17976aa652c5a796e1f0fdfe9005d3862e
Jonathan Essex <jonathan.essex@softwareplumbers.com>
2020-09-28 20:38:38 -0400

    Initial compiling version

--- src/main/java/com/softwareplumbers/feed/rest/client/spring/FeedServiceImpl.java
+++ src/main/java/com/softwareplumbers/feed/rest/client/spring/FeedServiceImpl.java
@@ -6,28 +6,40 @@
 package com.softwareplumbers.feed.rest.client.spring;
 
 import com.softwareplumbers.common.pipedstream.InputStreamSupplier;
+import com.softwareplumbers.feed.Cluster;
+import com.softwareplumbers.feed.Feed;
 import com.softwareplumbers.feed.FeedExceptions;
+import com.softwareplumbers.feed.FeedExceptions.InvalidPath;
 import com.softwareplumbers.feed.FeedExceptions.RemoteException;
 import com.softwareplumbers.feed.FeedExceptions.ServerError;
 import com.softwareplumbers.feed.FeedPath;
 import com.softwareplumbers.feed.FeedService;
+import com.softwareplumbers.feed.FeedServiceManager;
+import com.softwareplumbers.feed.Filters;
 import com.softwareplumbers.feed.Message;
 import com.softwareplumbers.feed.MessageIterator;
+import com.softwareplumbers.feed.impl.FeedImpl;
 import com.softwareplumbers.feed.impl.MessageFactory;
 import com.softwareplumbers.feed.impl.MessageImpl;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.io.StringReader;
 import java.net.URI;
 import java.time.Instant;
+import java.util.Base64;
 import java.util.Collections;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Consumer;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.json.JsonWriter;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 import org.springframework.core.io.InputStreamResource;
@@ -54,7 +66,47 @@ public class FeedServiceImpl implements FeedService {
  
     private String feedsUrl;
     private LoginHandler loginHandler;
-    private Map<Consumer<MessageIterator>, ListenableFuture<MessageIterator>> callbacks = new ConcurrentHashMap<>();
+    private final MessageFactory factory = new MessageFactory();
+    
+    
+    private static class ServiceInfo {
+        public final Instant initTime;
+        public final UUID serviceId;
+        public ServiceInfo(JsonObject serviceInfo) {
+            this.initTime = FeedService.getInitTime(serviceInfo);
+            this.serviceId = FeedService.getServerId(serviceInfo);
+        }
+    }
+    
+    private ServiceInfo serviceInfo;
+    
+    private ServiceInfo getServiceInfo() {
+        if (serviceInfo == null) {
+            UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(feedsUrl);
+            builder.path("feeds");  
+            try {
+                serviceInfo = new ServiceInfo(getJson(builder.build().toUri()));
+            } catch(IOException ioe) {
+                throw FeedExceptions.runtime(ioe);
+            }
+        }
+        return serviceInfo;
+    }
+    
+    private <T> CompletableFuture<T> convert(ListenableFuture<T> listenableFuture) {
+        
+        CompletableFuture<T> completable = new CompletableFuture<T>() {
+            @Override
+            public boolean cancel(boolean mayInterruptIfRunning) {
+               boolean result = listenableFuture.cancel(mayInterruptIfRunning);
+               super.cancel(mayInterruptIfRunning);
+               return result;
+            }
+        };
+
+        listenableFuture.addCallback(completable::complete, completable::completeExceptionally);
+        return completable;
+    }
     
     /** Set the URL for the Doctane web service to be called.
      * 
@@ -90,7 +142,6 @@ public class FeedServiceImpl implements FeedService {
         this(null, null);
     }
     
-    private final MessageFactory factory = new MessageFactory();
     
     /** Send a JSON object to the server.
      * 
@@ -118,44 +169,57 @@ public class FeedServiceImpl implements FeedService {
         return LOG.exit(Json.createReader(new StringReader(response.getBody())).readObject());
     }
     
+    protected JsonObject getJson(URI uri) throws IOException {
+        LOG.entry(uri);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        loginHandler.applyCredentials(headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+
+        return LOG.exit(Json.createReader(new StringReader(response.getBody())).readObject());
+    }
+    
     /** Get Messages from the server.
      * 
      * @param uri URI from which we will request JSON data
      * @return Parsed JSON object send by server as response.
      */
-    protected MessageIterator getMessages(URI uri) {
+    protected MessageIterator getMessages(URI uri, Predicate<Message>... filters) {
         LOG.entry(uri);
 
         RestTemplate restTemplate = new RestTemplate();
+        Predicate<Message> filter = Stream.of(filters).reduce(m->true, Predicate::and); 
 
         return LOG.exit(restTemplate.execute(
             uri, 
             HttpMethod.GET, 
             request -> loginHandler.applyCredentials(request.getHeaders()), 
-            response -> extractMessages(response, true)
+            response -> extractMessages(response, true).filter(filter)
         ));        
     }
     
     /** Get Messages from the server.
      * 
      * @param uri URI from which we will request messages
-     * @param callback Callback function to receive messages
+     * @return promise of a future message iterator
      */
-    protected void getMessages(URI uri, Consumer<MessageIterator> callback) {
-        LOG.entry(uri, callback);
+    protected CompletableFuture<MessageIterator> getMessagesAsync(URI uri, Predicate<Message>... filters) {
+        LOG.entry(uri);
 
         AsyncRestTemplate restTemplate = new AsyncRestTemplate();
+        Predicate<Message> filter = Stream.of(filters).reduce(m->true, Predicate::and); 
 
         ListenableFuture<MessageIterator> result = restTemplate.execute(
                     uri, 
                     HttpMethod.GET, 
                     request -> loginHandler.applyCredentials(request.getHeaders()), 
-                    response -> extractMessages(response, true)
+                    response -> extractMessages(response, true).filter(filter)
             );
         
-        callbacks.put(callback, result);
-        result.addCallback(messages->receiveMessagesOn(callback, messages), error->handleErrorOn(error,callback));
-        LOG.exit();
+        return LOG.exit(convert(result));
     }
     
     public MessageIterator extractMessages(ClientHttpResponse response, boolean copyBuffer) {
@@ -176,19 +240,6 @@ public class FeedServiceImpl implements FeedService {
         } catch (IOException error) {
             return LOG.exit(MessageIterator.defer(FeedExceptions.runtime(error)));
         }
-    }
-    
-    public void receiveMessagesOn(Consumer<MessageIterator> callback, MessageIterator messages) {
-        LOG.entry(callback, messages);
-        callbacks.remove(callback);
-        callback.accept(messages);
-        LOG.exit();
-    }
-    
-    public void handleErrorOn(Throwable error, Consumer<MessageIterator> callback) {
-        callbacks.remove(callback);
-        if (error instanceof FeedExceptions.BaseRuntimeException) callback.accept(MessageIterator.defer((FeedExceptions.BaseRuntimeException)error));
-        LOG.error("Unhandled exception", error);
     }
     
     /** Convert a raw ClientHttpResponse into a ServerError exception 
@@ -242,53 +293,68 @@ public class FeedServiceImpl implements FeedService {
             return new RemoteException(new ServerError(e.getStatusText()));
         });
     }
+    
+    public static String encode(JsonArray array) {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream(); JsonWriter writer = Json.createWriter(bos)) {
+            writer.write(array);
+            return Base64.getUrlEncoder().encodeToString(bos.toByteArray());
+        } catch (IOException ioe) {
+            throw FeedExceptions.runtime(ioe);
+        }
+    }
         
     @Override
-    public void listen(FeedPath path, Instant from, Consumer<MessageIterator> callback) throws FeedExceptions.InvalidPath {
-        LOG.entry(path, from);
-            UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(feedsUrl);
-            builder.path("{path}");
-            builder.queryParam("from", from);
-            builder.queryParam("wait", "20");
-            getMessages(builder.buildAndExpand(path.toString()).toUri(), callback);
-        LOG.exit();
-    }
-
-    @Override
-    public void cancelCallback(FeedPath fp, Consumer<MessageIterator> callback) throws FeedExceptions.InvalidPath {
-        ListenableFuture<MessageIterator> future = callbacks.remove(callback);
-        future.cancel(true);
-    }
-
-    @Override
-    public MessageIterator sync(FeedPath path, Instant from) throws FeedExceptions.InvalidPath {
+    public CompletableFuture<MessageIterator> listen(FeedPath path, Instant from, UUID serverId, long timeoutMillis, Predicate<Message>... filters) throws FeedExceptions.InvalidPath {
         LOG.entry(path, from);
         UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(feedsUrl);
-        builder.path("{path}");
+        builder.path("feeds/{path}");
         builder.queryParam("from", from);
-        return LOG.exit(getMessages(builder.buildAndExpand(path.toString()).toUri()));
+        builder.queryParam("wait", timeoutMillis);
+        builder.queryParam("filters", encode(Filters.toJson(filters)));
+        return LOG.exit(getMessagesAsync(builder.buildAndExpand(path.toString()).toUri(), Filters.local(filters)));
     }
 
+    @Override
+    public MessageIterator search(FeedPath path, UUID serverId, Instant from, boolean fromInclusive, Optional<Instant> to, Optional<Boolean> toInclusive, Optional<Boolean> relay, Predicate<Message>... filters) throws FeedExceptions.InvalidPath {
+        LOG.entry(path, from);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(feedsUrl);
+        builder.path("feeds/{path}");
+        builder.queryParam("from", from);
+        if (fromInclusive) builder.queryParam("fromInclusive", true);
+        if (to.isPresent()) builder.queryParam("to", to.get());
+        if (toInclusive.isPresent()) builder.queryParam("toInclusive", toInclusive.get());
+        if (relay.isPresent()) builder.queryParam("relay", relay.get());
+        if (filters.length > 0) builder.queryParam("filters", encode(Filters.toJson(filters)));
+        return LOG.exit(getMessages(builder.buildAndExpand(path.toString()).toUri(), Filters.local(filters)));
+    }
+
+    private Message fromJson(JsonObject object) throws ServerError {
+        FeedPath name = Message.getName(object)
+            .orElseThrow(()->new ServerError("No name in message returned from server"));
+        String id = name.part.getId()
+            .orElseThrow(()->new ServerError("No id in message returned from server"));
+        return LOG.exit(new MessageImpl(
+            Message.getType(object),
+            id,
+            name,
+            Message.getSender(object).orElse(null),
+            Message.getTimestamp(object).orElse(Instant.now()),
+            Message.getServerId(object),
+            Message.getRemoteInfo(object),
+            Message.getHeaders(object),
+            Message.getLength(object),
+            null                 
+        ));
+    }
+    
     @Override
     public Message post(FeedPath path, Message message) throws FeedExceptions.InvalidPath {
         LOG.entry(path, message);
         try {
             UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(feedsUrl);
             builder.path("{path}");
-            JsonObject result = LOG.exit(sendStream(builder.buildAndExpand(path.toString()).toUri(), HttpMethod.POST, message.toStream()));
-            FeedPath name = Message.getName(result)
-                .orElseThrow(()->new ServerError("No name in message returned from server"));
-            String id = name.part.getId()
-                .orElseThrow(()->new ServerError("No id in message returned from server"));
-            return LOG.exit(new MessageImpl(
-                id,
-                name,
-                Message.getSender(result).orElse(null),
-                Message.getTimestamp(result).orElse(Instant.now()),
-                Message.getHeaders(result),
-                Message.getLength(result),
-                ()->message.getData()                 
-            ));
+            JsonObject result = sendStream(builder.buildAndExpand(path.toString()).toUri(), HttpMethod.POST, message.toStream());
+            return LOG.exit(fromJson(result));
         } catch (HttpStatusCodeException e) {
             switch (e.getStatusCode()) {
                 case NOT_FOUND:
@@ -304,7 +370,111 @@ public class FeedServiceImpl implements FeedService {
     }
 
     @Override
-    public void dumpState() {
+    public CompletableFuture<MessageIterator> watch(UUID watcherServerId, Instant after, long timeoutMillis) {
+        LOG.entry(watcherServerId, after);
+        final String WATCH_FILTER = encode(Filters.toJson(new Predicate[] { Filters.POSTED_LOCALLY }));
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(feedsUrl);
+        builder.path("feeds");
+        builder.queryParam("from", after);
+        builder.queryParam("wait", timeoutMillis);
+        builder.queryParam("filters", WATCH_FILTER);
+        return LOG.exit(getMessagesAsync(builder.build().toUri()));
+    }
+
+    @Override
+    public void setManager(FeedServiceManager manager) {
+        // Hmm...
+    }
+
+    @Override
+    public Instant getInitTime() {
+        return getServiceInfo().initTime;
+    }
+
+    @Override
+    public MessageIterator search(FeedPath messageId, Predicate<Message>... filters) throws FeedExceptions.InvalidPath, FeedExceptions.InvalidId {
+        LOG.entry(messageId, filters);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(feedsUrl);
+        builder.path("feeds/{path}");
+        if (filters.length > 0) builder.queryParam("filters", encode(Filters.toJson(filters)));
+        return LOG.exit(getMessages(builder.buildAndExpand(messageId.toString()).toUri(), Filters.local(filters)));
+    }
+
+    @Override
+    public Message replicate(Message message) throws FeedExceptions.InvalidState {
+        LOG.entry(message);
+        try {
+            UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(feedsUrl);
+            builder.path("{path}");
+            JsonObject result = sendStream(builder.buildAndExpand(message.getFeedName().toString()).toUri(), HttpMethod.PUT, message.toStream());
+            return LOG.exit(fromJson(result));
+        } catch (HttpStatusCodeException e) {
+            switch (e.getStatusCode()) {
+                default:
+                    throw getDefaultError(e);
+            }
+        }  catch (IOException e) {
+            throw FeedExceptions.runtime(e);
+        } catch (ServerError e) {
+            throw FeedExceptions.runtime(e);
+        }
+    }
+
+    @Override
+    public UUID getServerId() {
+        return getServiceInfo().serviceId;
+    }
+
+    @Override
+    public Optional<Cluster> getCluster() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Feed getFeed(FeedPath path) throws FeedExceptions.InvalidPath {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(feedsUrl);
+        builder.path("feeds");  
+        try {
+            return LOG.exit(FeedImpl.fromJson(getJson(builder.build().toUri())));
+        } catch(IOException ioe) {
+            throw FeedExceptions.runtime(ioe);
+        }
+    }
+
+    @Override
+    public Stream<Feed> getChildren(FeedPath path) throws FeedExceptions.InvalidPath {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(feedsUrl);
+        builder.path("feeds");  
+        try {
+            JsonObject feed = getJson(builder.build().toUri());
+            return LOG.exit(Feed.getChildren(feed, FeedImpl::fromJson));
+        } catch(IOException ioe) {
+            throw FeedExceptions.runtime(ioe);
+        }
+    }
+
+    @Override
+    public Stream<Feed> getFeeds() {
+        try {
+            Feed root = getFeed(FeedPath.ROOT);
+            return Stream.of(root).flatMap(feed->feed.getChildren(this));
+        } catch (InvalidPath e) {
+            throw FeedExceptions.runtime(e);
+        }
+    }
+
+    @Override
+    public Optional<Instant> getLastTimestamp(FeedPath path) throws FeedExceptions.InvalidPath {
+        return getFeed(path).getLastTimestamp();
+    }
+
+    @Override
+    public void dumpState(PrintWriter out) {
+        out.println(getServiceInfo());
+    }
+
+    @Override
+    public void close() throws Exception {
+        //hmm
     }
 }
